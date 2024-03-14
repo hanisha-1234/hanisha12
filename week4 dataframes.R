dbd285556adcb46330ec3b533240c83910db3915
hanisha-1234 <228w1a1272@vrsec.ac.in>
2024-03-14 11:03:14 +0530

    week-4 DATAFRAMES

---
+++ week4 dataframes.R
@@ -0,0 +1,18 @@
+# Create a data frame named dframe
+dframe <- data.frame(
+  Person =c("Ram", "Aruna", "Manoj", "Rani", "Vijay", "Bhanu"),
+  Sex = factor(c("M", "F", "M", "F", "M", "F"), levels = c("M", "F")),
+  Funny = factor(c("High", "Med", "Low", "High", "High", "Med"), ordered = TRUE, levels = c("Low", "Med", "High"))
+)
+
+# Print the data frame
+print(dframe)
+
+# Add an age column to the data frame
+dframe$Age <- c(41, 41, 15, 16, 21, 60)
+
+# Print the updated data frame
+print(dframe)
+
+#reordering
+dframe<-subset(dframe,select=c())
